635cc4a62612fca0af20be8acf417406e5199e30
jack.davis <jack.davis@akqa.com>
2017-10-27 11:30:18 +0100

    Animations

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,30 @@
-import $ from 'jquery';
 import { TimelineLite } from 'gsap';
+
+const staggerFrom = {
+  opacity: 0,
+  y: 5,
+  ease: 'easeInOut'
+};
+
+const staggerTo = {
+  opacity: 1,
+  y: 0,
+};
 
 class App {
   constructor() {
     this.setInternals();
+
+    this.setScene();
+  }
+
+  setScene() {
+    new TimelineLite()
+      .staggerFromTo(this.staggerElms, .4, staggerFrom, staggerTo, .1)
   }
 
   setInternals() {
-    this.$title = $('.jd-h-title');
+    this.staggerElms = document.querySelectorAll('.stagger-up');
   }
 }
 
