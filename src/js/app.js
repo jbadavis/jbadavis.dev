ed07e0c58e37ff65fcad768296619ec6788ec1b9
Jack Davis <jbad90@gmail.com>
2019-12-17 13:42:23 +0000

    Upgrade gsap

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,20 @@
-import { TimelineLite } from 'gsap';
+import { TimelineLite } from "gsap";
 
 function animate() {
-  const pageOverlay = document.querySelectorAll('.jd-page-overlay');
-  const headerImage = document.querySelectorAll('.jd-h-image');
-  const staggerElms = document.querySelectorAll('.stagger-up');
+  const pageOverlay = document.querySelectorAll(".jd-page-overlay");
+  const headerImage = document.querySelectorAll(".jd-h-image");
+  const staggerElms = document.querySelectorAll(".stagger-up");
 
   new TimelineLite()
-    .to(pageOverlay, 0.7, { opacity: 0, ease: Power2.easeIn })
-    .set(pageOverlay, { 'z-index': -1 })
+    .to(pageOverlay, 0.7, { opacity: 0, ease: "power2.easeIn" })
+    .set(pageOverlay, { "z-index": -1 })
     .to(headerImage, 0.7, { opacity: 1 })
     .staggerFromTo(
       staggerElms,
-      1, { opacity: 0, y: 40, },
-      {
-        opacity: 1,
-        y: 0,
-        ease: 'easeInOut',
-      },
-      0.1,
+      1,
+      { opacity: 0, y: 40 },
+      { opacity: 1, y: 0, ease: "easeInOut" },
+      0.1
     );
 }
 
