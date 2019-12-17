d457858b2b5dd4e1f82554ce87fadbb70dccc175
Jack Davis <jbad90@gmail.com>
2019-12-17 14:34:16 +0000

    Set content width
    The Gulp configuration is also formatted.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-const gulp = require('gulp');
-const browsersync = require('browser-sync').create();
-const webpack = require('webpack-stream');
-const del = require('del');
-const sass = require('gulp-sass');
-const cleanCss = require('gulp-clean-css');
+const gulp = require("gulp");
+const browsersync = require("browser-sync").create();
+const webpack = require("webpack-stream");
+const del = require("del");
+const sass = require("gulp-sass");
+const cleanCss = require("gulp-clean-css");
 
 function clean() {
-  return del(['./dist']);
+  return del(["./dist"]);
 }
 
 function copy() {
-  return gulp.src('./src/img/*').pipe(gulp.dest('./dist/img'));
+  return gulp.src("./src/img/*").pipe(gulp.dest("./dist/img"));
 }
 
 function browserSync(done) {
   browsersync.init({
     server: {
-      baseDir: './',
+      baseDir: "./"
     },
-    port: 3000,
+    port: 3000
   });
 
   done();
@@ -32,31 +32,32 @@ function browserSyncReload(done) {
 
 function javascript() {
   return gulp
-    .src('src/js/app.js')
-    .pipe(webpack(require('./webpack.config.js')))
-    .pipe(gulp.dest('dist/'))
+    .src("src/js/app.js")
+    .pipe(webpack(require("./webpack.config.js")))
+    .pipe(gulp.dest("dist/"))
     .pipe(browsersync.stream());
 }
 
 function css() {
   return gulp
-    .src('./src/scss/**/*.scss')
-    .pipe(sass({includePaths: ['node_modules']}))
-    .pipe(cleanCss({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./dist/'))
+    .src("./src/scss/**/*.scss")
+    .pipe(sass({ includePaths: ["node_modules"] }))
+    .pipe(cleanCss({ compatibility: "ie8" }))
+    .pipe(gulp.dest("./dist/"))
     .pipe(browsersync.stream());
 }
 
 function watch() {
-  gulp.watch('./src/img/**/*', gulp.series(copy));
-  gulp.watch('./src/scss/**/*', gulp.series(css));
-  gulp.watch('./src/js/**/*', gulp.series(javascript));
+  gulp.watch("./index.html", gulp.series(browserSyncReload));
+  gulp.watch("./src/img/**/*", gulp.series(copy));
+  gulp.watch("./src/scss/**/*", gulp.series(css));
+  gulp.watch("./src/js/**/*", gulp.series(javascript));
 }
 
 exports.watch = gulp.series(
   clean,
   gulp.parallel(copy, css, javascript),
-  gulp.parallel(watch, browserSync),
+  gulp.parallel(watch, browserSync)
 );
 
 exports.default = gulp.series(clean, gulp.parallel(copy, css, javascript));
