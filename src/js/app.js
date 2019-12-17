a639f6b420e05a4f9962a1ea8b187fc86009f794
Jack Davis <jbad90@gmail.com>
2019-12-17 11:27:53 +0000

    Remove tracking

--- src/js/app.js
+++ src/js/app.js
@@ -1,48 +1,26 @@
 import { TimelineLite } from 'gsap';
-import Tracking from './utils/tracking';
 
-const staggerFrom = {
-  opacity: 0,
-  y: 40,
-};
+function animate() {
+  const pageOverlay = document.querySelectorAll('.jd-page-overlay');
+  const headerImage = document.querySelectorAll('.jd-h-image');
+  const staggerElms = document.querySelectorAll('.stagger-up');
 
-const staggerTo = {
-  opacity: 1,
-  y: 0,
-  ease: 'easeInOut'
-};
-
-class App {
-  constructor() {
-
-    this.init();
-  }
-
-  initTracking() {
-    new Tracking();
-  }
-
-  setScene() {
-    new TimelineLite()
-      .to(this.pageOverlay, .7, { 'opacity': 0, 'ease':  Power2.easeIn })
-      .set(this.pageOverlay, { 'z-index': -1 })
-      .to(this.headerImage, .7, { 'opacity': 1 })
-      .staggerFromTo(this.staggerElms, 1, staggerFrom, staggerTo, .1);
-  }
-
-  setInternals() {
-    this.pageOverlay = document.querySelectorAll('.jd-page-overlay');
-    this.headerImage = document.querySelectorAll('.jd-h-image');
-    this.staggerElms = document.querySelectorAll('.stagger-up');
-  }
-
-  init() {
-    this.setInternals();
-
-    this.setScene();
-
-    this.initTracking();
-  }
+  new TimelineLite()
+    .to(pageOverlay, 0.7, { opacity: 0, ease: Power2.easeIn })
+    .set(pageOverlay, { 'z-index': -1 })
+    .to(headerImage, 0.7, { opacity: 1 })
+    .staggerFromTo(
+      staggerElms,
+      1, { opacity: 0, y: 40, },
+      {
+        opacity: 1,
+        y: 0,
+        ease: 'easeInOut',
+      },
+      0.1,
+    );
 }
 
-new App();
+(function main() {
+  animate();
+})();
