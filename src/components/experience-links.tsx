64196790fb09fcb936f3e3f57d7959cf380956c3
Jack Davis <jbadavis@proton.me>
2023-12-01 15:24:28 +0000

    Add social icons
    Additionally:
    
    - An ExperienceLinks component is created.
    - The page URL is added to the README.
    - The document title is updated.

---
+++ src/components/experience-links.tsx
@@ -0,0 +1,36 @@
+import {Text, Link, ListItem, UnorderedList} from '@chakra-ui/react';
+
+const ExperienceLinks = () => (
+	<>
+		<Text>
+			I've previously worked for{' '}
+			<Link href="https://red-badger.com" isExternal>
+				Red Badger
+			</Link>
+			,{' '}
+			<Link href="https://www.akqa.com/" isExternal>
+				AKQA
+			</Link>{' '}
+			and on sites including:
+		</Text>
+		<UnorderedList spacing="0.6em">
+			<ListItem>
+				<Link href="https://dis-loyalty.com/" isExternal>
+					Dis-Loyalty.
+				</Link>
+			</ListItem>
+			<ListItem>
+				<Link href="https://www.nandos.co.uk/order/search" isExternal>
+					Nando's Delievery.
+				</Link>
+			</ListItem>
+			<ListItem>
+				<Link href="https://www.fosterandpartners.com/" isExternal>
+					Foster + Partners.
+				</Link>
+			</ListItem>
+		</UnorderedList>
+	</>
+);
+
+export default ExperienceLinks;
