f493c57a9fb1123f20331ddf210a262d316cd043
Jack Davis <jbadavis@proton.me>
2023-12-02 09:31:55 +0000

    Add simple animation
    Adds a simple animation to the page stack group.
    
    Additionally adds:
    
    - A new Bio and Divider component.
    - A Memoji profile picture.
    - Updated bio content.

--- src/app.tsx
+++ src/app.tsx
@@ -1,34 +1,39 @@
-import {Box, Heading, Divider, Text, Link, VStack} from '@chakra-ui/react';
+import {useEffect, useState} from 'react';
+import {Box, VStack, SlideFade} from '@chakra-ui/react';
+import Bio from './components/bio';
 import SocialLinks from './components/social-links';
 import ExperienceLinks from './components/experience-links';
+import Divider from './components/divider';
 
 function App() {
+	const [loaded, setLoaded] = useState(false);
+
+	useEffect(() => {
+		setLoaded(true);
+	});
+
 	return (
 		<Box background="#F3CFC6">
-			<VStack
-				spacing="1em"
-				align="flex-start"
-				justify="center"
-				h="100vh"
-				w="80%"
-				m="0 auto"
-				maxW="600px"
+			<SlideFade
+				in={loaded}
+				transition={{enter: {duration: 1.2, easing: 'ease-out'}}}
+				offsetY="25px"
 			>
-				<Heading letterSpacing="wide" textTransform="uppercase">
-					Jack Davis
-				</Heading>
-				<Divider borderColor="black" w="50%" />
-				<Text>
-					I'm a London-based developer working at{' '}
-					<Link href="https://planes.studio" isExternal>
-						Planes ✈️
-					</Link>
-					.
-				</Text>
-				<ExperienceLinks />
-				<Divider borderColor="black" w="50%" />
-				<SocialLinks />
-			</VStack>
+				<VStack
+					spacing={['1em', '1.8em']}
+					align="flex-start"
+					justify="center"
+					h="100vh"
+					w="80%"
+					m="0 auto"
+					maxW="600px"
+				>
+					<Bio />
+					<ExperienceLinks />
+					<Divider />
+					<SocialLinks />
+				</VStack>
+			</SlideFade>
 		</Box>
 	);
 }
---
+++ src/components/bio.tsx
@@ -0,0 +1,37 @@
+import {Box, Heading, Text, Link} from '@chakra-ui/react';
+import meUrl from '../assets/me.png';
+import Divider from './divider';
+
+const Bio = () => (
+	<>
+		<Box
+			h="80px"
+			w="80px"
+			border="3px solid black"
+			borderRadius="50%"
+			overflow="hidden"
+			mb="1em"
+			bgImage={meUrl}
+			bgSize="cover"
+		/>
+		<Heading as="h1" letterSpacing="wide" textTransform="uppercase">
+			Jack Davis
+		</Heading>
+		<Divider />
+		<Text>
+			I'm a London-based{' '}
+			<Link
+				href="https://docs.publishing.service.gov.uk/manual/tech-lead-responsibilities.html"
+				isExternal
+			>
+				tech lead
+			</Link>{' '}
+			working at{' '}
+			<Link href="https://planes.studio" isExternal>
+				Planes ✈️
+			</Link>
+			.
+		</Text>
+	</>
+);
+export default Bio;
